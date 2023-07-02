2bd182999de07e5d90f87a1f7f7b526a7a4ecf36
Giov3ss <giovani.sp25@gmail.com>
2023-07-02 15:13:22 +0000

    adding content to the mobile-top-header

--- powerprotein/settings.py
+++ powerprotein/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
