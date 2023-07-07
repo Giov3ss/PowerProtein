da24e89d4b67f81ff7b8f2d011d3fae7d356b844
Giov3ss <giovani.sp25@gmail.com>
2023-07-07 10:48:58 +0000

    adding strip publick key

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NRBLdB55T1ltf8c9AHPnreDjY9bykBYVOYb0x4GvLIKJI6pCkFThnZxQvooQbFnBP5A2dSqfuTA5ocXJNa2HlGC00nWKKgbtH',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
