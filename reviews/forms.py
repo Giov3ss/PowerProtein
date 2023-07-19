7f43ce51add4b09030ff8a85d8c63a795410f25b
Giov3ss <giovani.sp25@gmail.com>
2023-07-19 15:23:15 +0000

    fixing a typo in froms.py, creating the base template to reviews app, setting url to the main-nav

--- reviews/forms.py
+++ reviews/forms.py
@@ -7,7 +7,7 @@ class ReviewsForm(forms.ModelForm):
 
     class Meta:
         model = Reviews
-        fields = ('review_title', 'service_review ', 'service_rating', 'featured_image')
+        fields = ('review_title', 'service_review', 'service_rating', 'featured_image')
 
     featured_image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
     
