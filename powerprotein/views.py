e2b88e7e217a6515cf0c39d4a87f4ae0e501ef7e
Giov3ss <giovani.sp25@gmail.com>
2023-07-31 14:58:35 +0000

    fixing errors pages urls and views.py

--- powerprotein/views.py
+++ powerprotein/views.py
@@ -6,12 +6,12 @@ def handler404(request, exception):
 
 def handler403(request, exception):
     """ Error Handler 403 - Forbidden Error Variations """
-    return render(request, "errors/403.html", status=404)
+    return render(request, "errors/403.html", status=403)
 
-def handler500(request, exception):
+def handler500(request):
     """ Error Handler 500 - Server Error """
-    return render(request, "errors/500.html", status=404)
+    return render(request, "errors/500.html", status=500)
 
 def handler400(request, exception):
     """ Error Handler 400 - Bad Request """
-    return render(request, "errors/400.html", status=404)
+    return render(request, "errors/400.html", status=400)
